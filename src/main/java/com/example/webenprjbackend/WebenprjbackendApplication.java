f45c55465775b0b124c6bf5709fe0ef7437d04e2
Stefan Rudolf <stefan.rudolf@nils.dog>
2025-09-16 18:37:58 +0200

    Initial commit

---
+++ src/main/java/com/example/webenprjbackend/WebenprjbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.webenprjbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebenprjbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebenprjbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webenprjbackend/WebenprjbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webenprjbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebenprjbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
