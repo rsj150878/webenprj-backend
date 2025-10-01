b630606f428f7422f087da6451ae9813b513b987
Stefan Rudolf <stefan.rudolf@nils.dog>
2025-10-01 18:01:07 +0200

    First commit

--- src/main/java/at/fhtw/webenprjbackend/WebenprjbackendApplication.java
+++ src/main/java/at/fhtw/webenprjbackend/WebenprjbackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.webenprjbackend;
+package at.fhtw.webenprjbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
