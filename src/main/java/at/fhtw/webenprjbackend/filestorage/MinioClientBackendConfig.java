7b801a8dfbf6339ab0803657afdf745525208bba
Stefan Rudolf <stefan.rudolf@nils.dog>
2025-11-27 19:01:25 +0100

    Minio-Init & setup bucket

---
+++ src/main/java/at/fhtw/webenprjbackend/filestorage/MinioClientBackendConfig.java
@@ -0,0 +1,18 @@
+package at.fhtw.webenprjbackend.filestorage;
+
+import io.minio.MinioClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioClientBackendConfig {
+
+    @Bean
+    public MinioClient getMinioClient() {
+        return MinioClient.builder()
+                .endpoint("http://127.0.0.1:9000")
+                .credentials("minioadmin", "minioadmin")
+                .build();
+    }
+
+}
