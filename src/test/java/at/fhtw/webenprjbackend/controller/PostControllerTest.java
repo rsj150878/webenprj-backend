4fde1f03ae7fab1b89682b159cccad74991b4006
WiIhelm <rogerwilhelm@hotmail.de>
2025-12-06 01:05:31 +0100

    refactor: Replace MockitoBean with MockBean in controller tests for consistency

--- src/test/java/at/fhtw/webenprjbackend/controller/PostControllerTest.java
+++ src/test/java/at/fhtw/webenprjbackend/controller/PostControllerTest.java
@@ -20,7 +20,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import at.fhtw.webenprjbackend.dto.PostResponse;
@@ -32,7 +32,7 @@ class PostControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private PostService postService;
 
     @Test
