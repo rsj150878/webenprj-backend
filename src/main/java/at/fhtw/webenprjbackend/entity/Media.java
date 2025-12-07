e957ce9745aacaceb1598fcb68e0aaabf5b38a81
Stefan Rudolf <stefan.rudolf@nils.dog>
2025-12-07 13:28:37 +0100

    Media Resource Permission

--- src/main/java/at/fhtw/webenprjbackend/entity/Media.java
+++ src/main/java/at/fhtw/webenprjbackend/entity/Media.java
@@ -1,23 +1,25 @@
 package at.fhtw.webenprjbackend.entity;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "media")
 public class Media {
 
@@ -33,4 +35,20 @@ public class Media {
 
     @Column(name="name")
     private String name;
+
+    @Column(name="create_user")
+    @CreatedBy
+    private UUID createUser;
+
+    @Column(name="update_user")
+    @LastModifiedBy
+    private UUID updateUser;
+
+    @Column(name="create_timestamp")
+    @CreatedDate
+    private LocalDateTime createTimestamp;
+
+    @Column(name="update_timestamp")
+    @LastModifiedDate
+    private LocalDateTime updateTimestamp;
 }
