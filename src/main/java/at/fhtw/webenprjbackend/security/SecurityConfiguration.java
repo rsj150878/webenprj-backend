d12d16ba654d1f31f329d9a2b5a351b3f84d966c
WiIhelm <rogerwilhelm@hotmail.de>
2026-01-13 19:26:59 +0100

    feat: Allow CORS preflight requests for all endpoints in SecurityConfiguration

--- src/main/java/at/fhtw/webenprjbackend/security/SecurityConfiguration.java
+++ src/main/java/at/fhtw/webenprjbackend/security/SecurityConfiguration.java
@@ -69,6 +69,9 @@ public class SecurityConfiguration {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> {
+                    // Allow CORS preflight requests
+                    auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+
                     auth
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
